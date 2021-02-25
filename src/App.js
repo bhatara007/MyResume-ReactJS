c463fad56a198b25bfae53235987a8b3356b0bf6
Bhatara Chaemchan <bhatara007@gmail.com>
2021-02-25 23:49:39 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Navbar from './components/Navbar';
 import Banner from './components/Banner'
 import Skills from './components/Project'
 import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 function App() {
     return (
